774b2c20f8c74590f36a0ad792fbc5da5f98aaa3
Ryan Riley <block2150@gmail.com>
2017-10-27 08:13:08 -0600

    setup on server

--- core/config/hosts.php
+++ core/config/hosts.php
@@ -3,6 +3,7 @@
 $hosts = [
     "dyonyx" => "clients/dyonyx",
     "dyonyx.block2150.com" => "clients/dyonyx",
+    "dyonyx.stage" => "clients/dyonyx",
     "www.dyonyx.com" => "clients/dyonyx",
     "dyonyx.com" => "clients/dyonyx"
 ];
