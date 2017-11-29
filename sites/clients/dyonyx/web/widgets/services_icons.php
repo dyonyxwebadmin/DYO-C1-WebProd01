6e4af6666a2c6cd797fa9b6daca7c074722adfca
Ryan Riley <block2150@gmail.com>
2017-11-28 21:58:25 -0700

    seo updatres

--- sites/clients/dyonyx/web/widgets/services_icons.php
+++ sites/clients/dyonyx/web/widgets/services_icons.php
@@ -4,7 +4,7 @@
               <!-- Icon Blocks -->
               <div class="text-center">
                 <span class="d-inline-block">                  
-                  <img class="img-fluid" src="/img/professional.png" style="width:50%">
+                  <img class="img-fluid" src="/img/professional.png" alt="IT Managed Services" style="width:50%">
                 </span>
                 <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">IT MANAGED SERVICES</h3>
                 <p class="mb-0">Our managed IT services will help you enhance business operations, track people processes and assets, and improve customer satisfaction through the deployment of cloud technologies.</p>
@@ -19,7 +19,7 @@
               <!-- Icon Blocks -->
               <div class="text-center">
                 <span class="d-inline-block">                  
-                  <img class="img-fluid" src="/img/cloud.png" style="width:50%">
+                  <img class="img-fluid" src="/img/cloud.png" alt="Cloud Services" style="width:50%">
                 </span>
                 <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">Cloud Services</h3>
                 <p class="mb-0">DYONYX Cloud Solutions can run your entire business IT environment and offer you an affordable, flexible solution for storing data and connecting your offices and clients around the country and around the world.</p>
@@ -34,7 +34,7 @@
               <!-- Icon Blocks -->
               <div class="text-center">
                 <span class="d-inline-block">                  
-                  <img class="img-fluid" src="/img/security.png" style="width:50%">
+                  <img class="img-fluid" src="/img/security.png" alt="Security Services" style="width:50%">
                 </span>
                 <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">Security Services</h3>
                 <p class="mb-0">We specialize in design-level network security, working with agencies and executive management to help improve the security and efficiency of your existing infrastructures, systems and databases.</p>
@@ -49,7 +49,7 @@
               <!-- Icon Blocks -->
               <div class="text-center">
                 <span class="d-inline-block">                  
-                  <img class="img-fluid" src="/img/services.png" style="width:50%">
+                  <img class="img-fluid" src="/img/services.png" alt="Professional Services" style="width:50%">
                 </span>
                 <h3 class="h5 g-color-gray-dark-v2 g-font-weight-600 text-uppercase mb-3">Professional Services</h3>
                 <p class="mb-0">Whether you are looking to upgrade hardware or a specific engineered solution, our Professional Services Team can help you integrate and leverage technology while enhancing shareholder value.</p>
