d66545d2858863305ce04e9531e15b9ed6eb2aeb
Ryan Riley <block2150@gmail.com>
2018-04-30 10:27:18 -0600

    udpate

--- sites/clients/dyonyx/web/thankyou.php
+++ sites/clients/dyonyx/web/thankyou.php
@@ -18,7 +18,21 @@
 
       <?php includes("header") ?>
 
-      <?php widget("home_slider") ?>
+     
+      <!-- Page Header -->
+      <section class="dzsparallaxer auto-init height-is-based-on-content use-loading mode-scroll loaded dzsprx-readyall " data-options='{direction: "fromtop", animation_duration: 25, direction: "reverse"}'>
+        <!-- Parallax Image -->
+        <div class="divimage dzsparallaxer--target w-100 g-bg-cover g-bg-pos-bottom-center  g-bg-black-opacity-0_5--after" style="height: 130%; background-image: url(/img/bg/2.jpg);"></div>
+        <!-- End Parallax Image -->
+
+        <!-- Header Content -->
+        <div class="container g-color-white text-center g-py-200">
+          <h3 class="h2 g-font-weight-300 mb-0">Thank you for your answers, for immediate support please call</h3>
+          <h1 class="g-font-weight-600 g-font-size-55 text-uppercase">855-749-6758</h1>
+        </div>
+        <!-- End Header Content -->
+      </section>
+      <!-- End Page Header -->
       
      <?php widget("thank_you") ?>
 
